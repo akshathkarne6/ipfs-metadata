2934d1b7d19d3f46190d29dd21938730b018f7dd
akhil8521 <aj8521@gmail.com>
2024-08-14 13:37:38 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,138 +1,153 @@
-resource "aws_vpc" "main" {
- cidr_block           = "10.0.0.0/16"
- enable_dns_hostnames = true
- tags = {
-   name = "main"
- }
+provider "aws" {
+  region = "us-east-1"  
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main"
+  }
+}
+
 resource "aws_subnet" "subnet" {
- vpc_id                  = aws_vpc.main.id
- cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
- map_public_ip_on_launch = true
- availability_zone       = "us-west-2a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet2" {
- vpc_id                  = aws_vpc.main.id
- cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
- map_public_ip_on_launch = true
- availability_zone       = "us-west-2b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
 }
+
 resource "aws_internet_gateway" "internet_gateway" {
- vpc_id = aws_vpc.main.id
- tags = {
-   Name = "internet_gateway"
- }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "internet_gateway"
+  }
 }
+
 resource "aws_route_table" "route_table" {
- vpc_id = aws_vpc.main.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_route_table_association" "subnet2_route" {
- subnet_id      = aws_subnet.subnet2.id
- route_table_id = aws_route_table.route_table.id
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_security_group" "security_group" {
- name   = "ecs-security-group"
- vpc_id = aws_vpc.main.id
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.main.id
 
- ingress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = -1
-   self        = "false"
-   cidr_blocks = ["0.0.0.0/0"]
-   description = "any"
- }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = false
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "any"
+  }
 
- egress {
-   from_port   = 0
-   to_port     = 0
-   protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
 resource "aws_launch_template" "ecs_lt" {
- name_prefix   = "ecs-template"
- image_id      = "ami-0a38c1c38a15fed74"
- instance_type = "t3.micro"
+  name_prefix   = "ecs-template"
+  image_id      = "ami-0a38c1c38a15fed74"
+  instance_type = "t3.micro"
 
- key_name               = "ec2ecsglog"
- vpc_security_group_ids = [aws_security_group.security_group.id]
- iam_instance_profile {
-   name = "ecsInstanceRole"
- }
+  key_name               = "ec2ecsglog"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
 
- block_device_mappings {
-   device_name = "/dev/xvda"
-   ebs {
-     volume_size = 30
-     volume_type = "gp2"
-   }
- }
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
 
- tag_specifications {
-   resource_type = "instance"
-   tags = {
-     Name = "ecs-instance"
-   }
- }
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
 
- user_data = filebase64("${path.module}/ecs.sh")
+  user_data = filebase64("${path.module}/ecs.sh")
 }
+
 resource "aws_autoscaling_group" "ecs_asg" {
- vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
- desired_capacity    = 2
- max_size            = 3
- min_size            = 1
+  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
 
- launch_template {
-   id      = aws_launch_template.ecs_lt.id
-   version = "$Latest"
- }
+  launch_template {
+    id      = aws_launch_template.ecs_lt.id
+    version = "$Latest"
+  }
 
- tag {
-   key                 = "AmazonECSManaged"
-   value               = true
-   propagate_at_launch = true
- }
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = "true"
+    propagate_at_launch = true
+  }
 }
-resource "aws_lb" "ecs_alb" {
- name               = "ecs-alb"
- internal           = false
- load_balancer_type = "application"
- security_groups    = [aws_security_group.security_group.id]
- subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
 
- tags = {
-   Name = "ecs-alb"
- }
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+
+  tags = {
+    Name = "ecs-alb"
+  }
 }
 
 resource "aws_lb_listener" "ecs_alb_listener" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 80
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
- name        = "ecs-target-group"
- port        = 80
- protocol    = "HTTP"
- target_type = "ip"
- vpc_id      = aws_vpc.main.id
+  name        = "ecs-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
 
- health_check {
-   path = "/"
- }
+  health_check {
+    path = "/"
+  }
 }
